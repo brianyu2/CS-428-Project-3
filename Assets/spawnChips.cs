be5c9df0d33e9527808098e24da242fbe2af02e0
brianyu2 <59840881+brianyu2@users.noreply.github.com>
2020-11-28 17:36:06 -0600

    Initial commit

---
+++ Assets/spawnChips.cs
@@ -0,0 +1,30 @@
+﻿namespace VRTK.Examples
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+
+
+    public class spawnChips : MonoBehaviour
+    {
+        public Transform startPosRot;
+        public GameObject chips;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 posAdd;
+            
+            posAdd = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+            Instantiate(chips, startPosRot.position + posAdd, startPosRot.rotation);
+        }
+    }
+
+}
