f11d9ff9b831c79d5e7b0ed1daa3babd149ae359
brianyu2 <59840881+brianyu2@users.noreply.github.com>
2020-11-28 19:18:15 -0600

    added animation somewhat

---
+++ Assets/ChangeState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeState : MonoBehaviour
+{
+    Animator anima;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anima = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anima.SetTrigger("wave");
+        }
+    }
+}
