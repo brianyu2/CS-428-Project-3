4fc433b87cf7f010ba6a9bb0f57ca4efde30bb48
brianyu2 <59840881+brianyu2@users.noreply.github.com>
2020-11-28 18:35:15 -0600

    smallupdate

--- Assets/spawnChips.cs
+++ Assets/spawnChips.cs
@@ -1,30 +1,38 @@
-﻿namespace VRTK.Examples
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class spawnChips : MonoBehaviour
 {
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
+    public Transform startPosRot;
+    public GameObject chips;
+    private float waitTime = 2.0f;
+    private float timer = 0.0f;
 
-
-
-    public class spawnChips : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
-        public Transform startPosRot;
-        public GameObject chips;
 
-        // Start is called before the first frame update
-        void Start()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetMouseButton(1) && timer > waitTime)
         {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            Vector3 posAdd;
-            
-            posAdd = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
-            Instantiate(chips, startPosRot.position + posAdd, startPosRot.rotation);
+            timer = 0.0f;
+            spawn();
         }
     }
 
+    public void spawn()
+    {
+        Vector3 posAdd;
+        posAdd = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+        Instantiate(chips, startPosRot.position + posAdd, startPosRot.rotation);
+    }
 }
+
